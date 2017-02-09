04615ae175c24bfb52d383814fdf2264bc3f8d05
radiantwf <radiantwf@gmail.com>
2017-02-09 16:20:04 +0800

    websocket配置

--- filesystem.sh
+++ filesystem.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker kill seaweedfs-redis seaweedfs-master1 seaweedfs-volume1 seaweedfs-filer1 getmapservice;docker rm seaweedfs-redis seaweedfs-master1 seaweedfs-volume1 seaweedfs-filer1 getmapservice;
+docker rmi 211.157.146.6:5000/getmapservice
 
 docker run -d --name seaweedfs-redis 211.157.146.6:5000/redis
 docker run -d -p 9333:9333 --name="seaweedfs-master1" 211.157.146.6:5000/seaweedfs master
@@ -7,3 +8,10 @@ docker run -d -p 8888:8080 --link="seaweedfs-master1:master" --name="seaweedfs-v
 docker run -d -p 8889:8080 --link="seaweedfs-master1:master" --link="seaweedfs-redis:redis" --name="seaweedfs-filer1" 211.157.146.6:5000/seaweedfs filer -redis.server="redis:6379" -master="master:9333" -port=8080
 docker run -d --link "seaweedfs-filer1:filer" -p 8000:8000 --name="getmapservice" 211.157.146.6:5000/getmapservice
 
+docker kill getmapservice;docker rm getmapservice;
+docker rmi 211.157.146.6:5000/getmapservice
+docker run -d --link "seaweedfs-filer1:filer" -p 8000:8000 --name="getmapservice" 211.157.146.6:5000/getmapservice
+
+docker kill mapserver-nginx;docker rm mapserver-nginx;
+docker rmi 211.157.146.6:5000/mapserver-nginx
+docker run -d -p 6003:6003 -p 6004:6004 --name="mapserver-nginx" 211.157.146.6:5000/mapserver-nginx
